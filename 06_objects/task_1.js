d59125cf43730f760577a5df2c4e2d9672a6eca9
Zybin Aleksander <zybin.al@hotmail.com>
2023-07-15 22:44:25 +0300

    made hmwrk for task 6

--- 06_objects/task_1.js
+++ 06_objects/task_1.js
@@ -1,16 +1,20 @@
 // Обязательная часть задания
 let user1={
   name: 'Игорь',
-  age: 17
+  age: 99
 }
 
 let user2={
   name: 'Оля',
-  age: 21
+  age: 1
 }
 
 function getOlderUser(userOne, userTwo){
-  // Здесь решение задачи
+  if (user1.age > user2.age){
+    return user1.name
+  }
+  else
+    return user2.name;
 }
 
 let result = getOlderUser(user1, user2)
@@ -18,32 +22,40 @@ console.log('Старший пользователь:',result);
 
 
 // // Не обязательная часть задания
-// let allUsers=[
-//   {
-//     name: 'Валя',
-//     age: 11
-//   },
-//   {
-//     name: 'Таня',
-//     age: 24
-//   },
-//   {
-//     name: 'Рома',
-//     age: 21
-//   },
-//   {
-//     name: 'Надя',
-//     age: 34
-//   },
-//   {
-//     name: 'Антон',
-//     age: 7
-//   }
-// ]
+let allUsers=[
+  {
+    name: 'Валя',
+    age: 11
+  },
+  {
+    name: 'Таня',
+    age: 24
+  },
+  {
+    name: 'Рома',
+    age: 21
+  },
+  {
+    name: 'Надя',
+    age: 34
+  },
+  {
+    name: 'Антон',
+    age: 7
+  }
+]
 
-// function getOlderUserArray(usersArray){
-//   // Здесь решение задачи
-// }
+function getOlderUserArray(usersArray){
+let maxId = 0;
+let max = usersArray[0].age;
+for (i = 1; i < usersArray.length; i++) {
+  if (usersArray[i].age > max) {
+    max = usersArray[i].age;
+    maxId = i;
+  }
+}
+return usersArray[maxId].name;
+}
 
-// let result2 = getOlderUserArray(allUsers)
-// console.log('Старший пользователь:',result2);
+let result2 = getOlderUserArray(allUsers)
+console.log('Старший пользователь:',result2);
--- 06_objects/task_3.js
+++ 06_objects/task_3.js
@@ -1,16 +1,36 @@
 let objects = [
   { name: 'Василий', surname: 'Васильев' },
   { name: 'Иван', surname: 'Иванов' },
-  { name: 'Пётр', surname: 'Петров' }
+  { name: 'Пётр', surname: 'Петров' },
 ]
- 
+
 
 function filter(arr, prop, value){
-  // Здесь решение задачи
-  // arr - массив объектов
-  // prop - свойство по которому производится фильтрация
-  // value - значение свойства по которому производится фильтрация
+let emptyArray= [];
+for (i = 0; i < arr.length; i++) {
+  let entries = Object.entries(arr[i]);
+ for (let [keys, values] of entries) {
+   if (keys === prop && values === value) {
+    emptyArray.push(arr[i])
+   } ;
+ }
 }
-
+return emptyArray
+}
 let result = filter(objects, 'name', 'Иван');
 console.log(result);
+
+// Решил 2 способами, какой из них на реальной практике будет лучше, чтобы им воспользоваться? (2 способ указал ниже)
+
+// function filter(arr, prop, value){
+//   let emptyArray= [];
+//   for (i = 0; i < arr.length; i++) {
+//     if (arr[i][prop] === value) {
+//       emptyArray.push(arr[i]);
+//     }
+//   }
+//   return emptyArray;
+// }
+//   let result = filter(objects, 'name', 'Иван');
+//   console.log(result);
+
