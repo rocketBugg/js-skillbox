6db3da9b213af946d435dd48d4714d16443905a5
Skillbox <it@skillbox.ru>
2023-07-15 15:48:26 +0300

    Add lesson

---
+++ 06_objects/task_3.js
@@ -0,0 +1,16 @@
+let objects = [
+  { name: 'Василий', surname: 'Васильев' },
+  { name: 'Иван', surname: 'Иванов' },
+  { name: 'Пётр', surname: 'Петров' }
+]
+ 
+
+function filter(arr, prop, value){
+  // Здесь решение задачи
+  // arr - массив объектов
+  // prop - свойство по которому производится фильтрация
+  // value - значение свойства по которому производится фильтрация
+}
+
+let result = filter(objects, 'name', 'Иван');
+console.log(result);
